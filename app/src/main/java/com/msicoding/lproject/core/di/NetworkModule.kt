5d648468fd68febd2fa4f2b5c41a8733307d5288
msiprime <msisakib958@gmail.com>
2023-11-08 23:22:56 +0600

    wtf how is it working?

---
+++ app/src/main/java/com/msicoding/lproject/core/di/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.msicoding.lproject.core.di
+
+import com.msicoding.lproject.data.remote.WeatherService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherService(retrofit: Retrofit): WeatherService {
+        return retrofit.create(WeatherService::class.java)
+    }
+}
+
